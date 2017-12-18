d2e5013ca31ec567578037dc03e1a415c3cfaf88
BlaineConnaughton <BlaineConnaughton@users.noreply.github.com>
2017-12-18 10:07:42 -0500

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import os
+
+
+def post_to_splunk(payload):
+
+	token = "Splunk " + os.environ['http_token']
+	#url = "http://localhost:8088/services/collector"
+	url = os.environ['URL']
+	headers = {'Authorization': token }
+	return requests.post(url=url, data=payload, headers=headers)
+
+def package_data(payload):
+
+	data = {'event' : payload}
+	response = post_to_splunk(json.dumps(data))
+	return response.status_code
+
+def lambda_handler(event, context):
+	
+	alerts = event["alerts"]
+	status = []
+	for alert in alerts:
+		status.append(package_data(alert))
+	
+	return status
